ced7a638761bc2b22b53f044b635f4153e335740
ferminaguilar <fermin@ferminaguilar.com>
2026-08-04 20:56:21 -0600

    theme

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = 'e814ce35ce80322a';
+  public const string VERSIONS_HASH = '53c17999f022ffd3';
 
 }
