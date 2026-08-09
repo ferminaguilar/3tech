c67a0b6f3c5d0ec71be785a91e235cffc7148467
ferminaguilar <fermin@ferminaguilar.com>
2026-08-09 13:05:06 -0600

    adding configuration for uswds sass

--- themes/custom/government/gulpfile.js
+++ themes/custom/government/gulpfile.js
@@ -1,26 +1,26 @@
 /* gulpfile.js */
 
-// eslint-disable-next-line import/no-unresolved
 const uswds = require('@uswds/compile');
 
 /**
  * USWDS version
  */
-
 uswds.settings.version = 3;
 
 /**
  * Path settings
- * Set as many as you need
  */
-
 uswds.paths.dist.css = './css';
 uswds.paths.dist.theme = './sass';
+uswds.paths.dist.img = './assets/img';
+uswds.paths.dist.fonts = './assets/fonts';
+uswds.paths.dist.js = './assets/js';
 
 /**
  * Exports
- * Add as many as you need
  */
-
 exports.init = uswds.init;
 exports.compile = uswds.compile;
+exports.watch = uswds.watch;
+exports.default = uswds.watch;
+
